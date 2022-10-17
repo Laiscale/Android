0d95de1aba131460fb61cd4b5d8933c06f198e11
“leqaqin <leqaqin@gmail.com>
2022-10-17 16:48:21 +0800

    leqaqin 10 .17

--- discover/src/main/java/com/example/discover/DiscoverFragment.java
+++ discover/src/main/java/com/example/discover/DiscoverFragment.java
@@ -1,68 +1,119 @@
 package com.example.discover;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.fragment.app.Fragment;
+import com.example.common.MyApp;
+import com.example.common.adapter.PicRecyclerViewAdapter;
+import com.example.common.bean.PictureData;
+import com.example.common.bean.ResponseData;
+import com.example.common.ui.details.PicDetailsFragment;
+import com.example.common.listener.OnItemClickListener;
+import com.example.common.listener.RecyclerViewItemListener;
+import com.example.common.util.FragmentStackUtil;
+import com.example.discover.databinding.FragmentDiscoverBinding;
+import com.scwang.smart.refresh.footer.ClassicsFooter;
+import com.scwang.smart.refresh.header.ClassicsHeader;
+import com.scwang.smart.refresh.layout.api.RefreshLayout;
+import com.scwang.smart.refresh.layout.listener.OnLoadMoreListener;
+import com.scwang.smart.refresh.layout.listener.OnRefreshListener;
 
-import java.nio.channels.ClosedByInterruptException;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DiscoverFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class DiscoverFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public DiscoverFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DiscoverFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DiscoverFragment newInstance(String param1, String param2) {
-        DiscoverFragment fragment = new DiscoverFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private FragmentDiscoverBinding viewBinding;
+    private DiscoverViewModel viewModel;
+    private List<PictureData> pictureDataList = new ArrayList<>();
+    private PicRecyclerViewAdapter recyclerViewAdapter = new PicRecyclerViewAdapter(pictureDataList);
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        //bug-fit
-        //mParam1 = getArguments().getString(ARG_PARAM1);
-        //            mParam2 = getArguments().getString(ARG_PARAM2);
-        return inflater.inflate(R.layout.fragment_discover, container, false);
+        viewBinding = FragmentDiscoverBinding.inflate(inflater, container, false);
+        viewModel = new ViewModelProvider(this).get(DiscoverViewModel.class);
+        // 配置 Adapter
+        viewBinding.discoverRecyclerView.setAdapter(recyclerViewAdapter);
+        // 定义瀑布布局
+        viewBinding.discoverRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
+        viewModel.getDiscoverPicList(1, 10, MyApp.getUserBean().id);
+
+        viewBinding.smartRefresh.setEnableAutoLoadMore(true);
+        viewBinding.smartRefresh.setRefreshHeader(new ClassicsHeader(requireContext()));
+        viewBinding.smartRefresh.setRefreshFooter(new ClassicsFooter(requireContext()));
+
+        viewBinding.smartRefresh.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh(@NonNull RefreshLayout refreshLayout) {
+                if (viewModel.getHasMore()){
+                    viewModel.getDiscoverPicList(viewModel.getCurrentPage() + 1, 10, MyApp.getUserBean().id);
+                }else {
+                    // 如果没有更多，停止加载动画
+                    viewBinding.smartRefresh.finishRefresh();
+                }
+            }
+        });
+
+        viewBinding.smartRefresh.setOnLoadMoreListener(new OnLoadMoreListener() {
+            @Override
+            public void onLoadMore(@NonNull RefreshLayout refreshLayout) {
+                if (viewModel.getHasMore()){
+                    viewModel.getDiscoverPicList(viewModel.getCurrentPage() + 1, 10, MyApp.getUserBean().id);
+                }else {
+                    // 如果没有更多，停止加载动画
+                    viewBinding.smartRefresh.finishLoadMore();
+                }
+            }
+        });
+
+        viewModel.discoverPicList.observe(getViewLifecycleOwner(), new Observer<ResponseData<PictureData>>() {
+            @Override
+            public void onChanged(ResponseData<PictureData> pictureDataResponseData) {
+                // 结束刷新
+                viewBinding.smartRefresh.finishLoadMore();
+                viewBinding.smartRefresh.finishRefresh();
+                // 如果data为null，就直接返回
+                if (pictureDataResponseData == null) return;
+                if (!pictureDataResponseData.records.isEmpty()){
+                    if (viewModel.getCurrentPage() == 1){
+                        pictureDataList.addAll(0, pictureDataResponseData.records);
+                        // 通知刷新
+                        recyclerViewAdapter.notifyItemRangeChanged(0, pictureDataResponseData.size);
+                    }else {
+                        pictureDataList.addAll(pictureDataResponseData.records);
+                        // 通知刷新
+                        recyclerViewAdapter.notifyItemRangeChanged((viewModel.getCurrentPage() - 1) * 10, pictureDataResponseData.size);
+                    }
+                }
+            }
+        });
+
+        viewBinding.discoverRecyclerView.addOnItemTouchListener(new RecyclerViewItemListener(requireContext(), viewBinding.discoverRecyclerView, new OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                // 跳转到详情页
+                FragmentStackUtil.addToMainFragment(requireActivity().getSupportFragmentManager(), PicDetailsFragment.newInstance(pictureDataList.get(position)), "PicDetail_Fragment_Tag", true, "PicDetail_Fragment");
+            }
+
+            @Override
+            public void onItemLongClick(View view, int position) {
+
+            }
+        }));
+        return viewBinding.getRoot();
     }
 }
