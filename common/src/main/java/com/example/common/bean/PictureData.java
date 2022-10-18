c9db7cc72e266b3103b7ca963489d40b35f9b064
“leqaqin <leqaqin@gmail.com>
2022-10-18 22:23:36 +0800

    leqaqin 10 .17 fit

--- common/src/main/java/com/example/common/bean/PictureData.java
+++ common/src/main/java/com/example/common/bean/PictureData.java
@@ -4,19 +4,19 @@ import java.io.Serializable;
 import java.util.List;
 
 public class PictureData implements Serializable {
-   public long collectId;
-   public int collectNum;
-   public String content;
-   public long createTime;
-   public Boolean hasCollect;
-   public Boolean hasFocus;
-   public Boolean hasLike;
-   public long id;
-   public long imageCode;
-   public int likeId;
-   public int likeNum;
-   public long pUserId;
-   public String title;
-   public String username;
-   public List<String> imageUrlList;
+    public long collectId;
+    public int collectNum;
+    public String content;
+    public long createTime;
+    public Boolean hasCollect;
+    public Boolean hasFocus;
+    public Boolean hasLike;
+    public long id;
+    public long imageCode;
+    public int likeId;
+    public int likeNum;
+    public long pUserId;
+    public String title;
+    public String username;
+    public List<String> imageUrlList;
 }
